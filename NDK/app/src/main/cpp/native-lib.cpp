77c37c5f7c127a590d6d12dc9ad8720ddd88b18b
손소독 <vot768@naver.com>
2022-08-09 22:22:23 +0900

    1. cpp 안드로이드 디버그 로그

--- NDK/app/src/main/cpp/native-lib.cpp
+++ NDK/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,23 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_vot_ndk_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        JNIEnv *env,
+        jobject /* this */,
+        jstring prompt,
+        jint value
+) {
+    const char *str = env->GetStringUTFChars(prompt, NULL);
+    if (str == NULL) return NULL;
+
+    // char type의 변수에서 사용한 메모리는 함수의 제어를 반환하기 전에 해제
+    env->ReleaseStringUTFChars(prompt, str);
+
+    // external fun으로 받은 String과 Int 타입의 데이터를 Logcat에 띄움
+    __android_log_print(ANDROID_LOG_INFO, "App Debug", "%s(default %d) : ", str, value);
+
+    const char *buf = "손소독";
+    return env->NewStringUTF(buf);
 }
--- NDK/app/src/main/java/com/vot/ndk/MainActivity.kt
+++ NDK/app/src/main/java/com/vot/ndk/MainActivity.kt
@@ -15,15 +15,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        val textView: TextView = findViewById(R.id.sample_text)
+
+        var input: String = stringFromJNI("입력 값: ", 100)
+        textView.text = input
     }
 
     /**
      * A native method that is implemented by the 'ndk' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
+    external fun stringFromJNI(prompt: String, value: Int): String
 
     companion object {
         // Used to load the 'ndk' library on application startup.
